e5ade087f4271e313457ccff05177d420af98548
Bill Erickson <bill.erickson@gmail.com>
2020-04-06 12:53:11 -0500

    updates from cultivate

--- category-landing-page.php
+++ category-landing-page.php
@@ -1,29 +1,34 @@
 <?php
 /**
- * Plugin Name: Category Landing Page
- * Plugin URI:	https://github.com/billerickson/Category-Landing-Page/
- * Description: Turn your category archives into rich, engaging, SEO friendly landing pages.
- * Version:     1.0.0
- * Author:      Bill Erickson
- * Author URI:  https://www.billerickson.net
- * Requires at least: 5.0
- * License:     MIT
- * License URI: http://www.opensource.org/licenses/mit-license.php
- */
+ * Category Landing Page
+ * If you would like to use this as a standalone plugin: https://github.com/billerickson/Category-Landing-Page/
+ * Requires Advanced Custom Fields plugin.
+ *
+ * @package      Cultivate
+ * @author       Bill Erickson
+ * @since        1.0.0
+ * @license      GPL-2.0+
+**/
 
- class Category_Landing_Page {
+class Category_Landing_Page {
 
- 	/**
- 	 * Instance of the class.
- 	 * @var object
- 	 */
- 	private static $instance;
+	/**
+	 * Instance of the class.
+	 * @var object
+	 */
+	private static $instance;
 
- 	/**
- 	 * Supported taxonomies
- 	 * @var array
- 	 */
- 	public $supported_taxonomies;
+	/**
+	 * Supported taxonomies
+	 * @var array
+	 */
+	public $supported_taxonomies;
+
+	/**
+	 * Supported CPT Archives
+	 * @var array
+	 */
+	public $supported_cpt_archives;
 
 	/**
 	 * Post type
@@ -31,22 +36,27 @@
 	 */
 	public $post_type = 'be_landing_page';
 
- 	/**
- 	 * Class Instance.
- 	 * @return Category_Landing_Page
- 	 */
- 	public static function instance() {
- 		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Category_Landing_Page ) ) {
- 			self::$instance = new Category_Landing_Page();
+	/**
+	 * Class Instance.
+	 * @return Category_Landing_Page
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof Category_Landing_Page ) ) {
+			self::$instance = new Category_Landing_Page();
 
 			add_action( 'init', [ self::$instance, 'supported_taxonomies' ], 4 );
- 			add_action( 'init', [ self::$instance, 'register_cpt' ], 12 );
+			add_action( 'init', [ self::$instance, 'supported_cpt_archives' ], 4 );
+			add_action( 'init', [ self::$instance, 'register_cpt' ], 12 );
 			add_action('acf/init', [ self::$instance, 'register_metabox' ] );
- 			add_action( 'admin_bar_menu', [ self::$instance, 'admin_bar_link_front' ], 90 );
+			add_action( 'admin_bar_menu', [ self::$instance, 'admin_bar_link_front' ], 90 );
 			add_action( 'admin_bar_menu', [ self::$instance, 'admin_bar_link_back' ], 90 );
- 		}
- 		return self::$instance;
- 	}
+
+			// Built-in support for Genesis and Theme Hook Alliance
+			add_action( 'tha_content_while_before', [ self::$instance, 'show' ], 20 );
+			add_action( 'genesis_before_while', [ self::$instance, 'show' ], 20 );
+		}
+		return self::$instance;
+	}
 
 	/**
 	 * Supported Taxonomies
@@ -56,44 +66,52 @@
 		$this->supported_taxonomies = apply_filters( 'category_landing_page_taxonomies', [ 'category' ] );
 	}
 
- 	/**
- 	 * Register the custom post type
- 	 *
- 	 */
- 	function register_cpt() {
+	/**
+	 * Supported Post Type Archives
+	 *
+	 */
+	function supported_cpt_archives() {
+		$this->supported_cpt_archives = apply_filters( 'category_landing_page_cpt_archives', [] );
+	}
 
- 		$labels = array(
- 			'name'               => 'Landing Pages',
- 			'singular_name'      => 'Landing Page',
- 			'add_new'            => 'Add New',
- 			'add_new_item'       => 'Add New Landing Page',
- 			'edit_item'          => 'Edit Landing Page',
- 			'new_item'           => 'New Landing Page',
- 			'view_item'          => 'View Landing Page',
- 			'search_items'       => 'Search Landing Pages',
- 			'not_found'          => 'No Landing Pages found',
- 			'not_found_in_trash' => 'No Landing Pages found in Trash',
- 			'parent_item_colon'  => 'Parent Landing Page:',
- 			'menu_name'          => 'Landing Pages',
- 		);
+	/**
+	 * Register the custom post type
+	 *
+	 */
+	function register_cpt() {
 
- 		$args = array(
- 			'labels'              => $labels,
- 			'hierarchical'        => false,
- 			'supports'            => array( 'title', 'editor', 'revisions' ),
- 			'public'              => false,
- 			'show_ui'             => true,
- 			'show_in_rest'	      => true,
- 			'exclude_from_search' => true,
- 			'has_archive'         => false,
- 			'query_var'           => true,
- 			'can_export'          => true,
- 			'rewrite'             => false,
- 			'menu_icon'           => 'dashicons-welcome-widgets-menus',
- 		);
+		$labels = array(
+			'name'               => 'Landing Pages',
+			'singular_name'      => 'Landing Page',
+			'add_new'            => 'Add New',
+			'add_new_item'       => 'Add New Landing Page',
+			'edit_item'          => 'Edit Landing Page',
+			'new_item'           => 'New Landing Page',
+			'view_item'          => 'View Landing Page',
+			'search_items'       => 'Search Landing Pages',
+			'not_found'          => 'No Landing Pages found',
+			'not_found_in_trash' => 'No Landing Pages found in Trash',
+			'parent_item_colon'  => 'Parent Landing Page:',
+			'menu_name'          => 'Landing Pages',
+		);
 
- 		register_post_type( $this->post_type, $args );
- 	}
+		$args = array(
+			'labels'              => $labels,
+			'hierarchical'        => false,
+			'supports'            => array( 'title', 'editor', 'revisions' ),
+			'public'              => false,
+			'show_ui'             => true,
+			'show_in_rest'	      => true,
+			'exclude_from_search' => true,
+			'has_archive'         => false,
+			'query_var'           => true,
+			'can_export'          => true,
+			'rewrite'             => false,
+			'menu_icon'           => 'dashicons-welcome-widgets-menus',
+		);
+
+		register_post_type( $this->post_type, $args );
+	}
 
 	/**
 	 * Register metabox
@@ -153,76 +171,93 @@
 	}
 
 
- 	/**
- 	 * Show landing page
- 	 *
- 	 */
- 	function show( $location = '' ) {
- 		if( ! $location )
- 			$location = $this->get_landing_id();
+	/**
+	 * Show landing page
+	 *
+	 */
+	function show( $location = '' ) {
+		if( ! $location )
+			$location = $this->get_landing_id();
 
 		if( empty( $location ) || get_query_var( 'paged' ) )
 			return;
 
- 		$args = [ 'post_type' => $this->post_type, 'posts_per_page' => 1 ];
- 		if( is_int( $location ) )
- 			$args['p'] = intval( $location );
- 		else
- 			$args['name'] = sanitize_key( $location );
+		$args = [ 'post_type' => $this->post_type, 'posts_per_page' => 1 ];
+		if( is_int( $location ) )
+			$args['p'] = intval( $location );
+		else
+			$args['name'] = sanitize_key( $location );
 
- 		$loop = new WP_Query( $args );
+		$loop = new WP_Query( $args );
 
- 		if( $loop->have_posts() ): while( $loop->have_posts() ): $loop->the_post();
- 			echo '<div class="block-area block-area-' . sanitize_key( get_the_title() ) . '">';
- 				the_content();
- 			echo '</div>';
- 		endwhile; endif; wp_reset_postdata();
- 	}
+		if( $loop->have_posts() ): while( $loop->have_posts() ): $loop->the_post();
+			echo '<div class="block-area block-area-' . sanitize_key( get_the_title() ) . '">';
+				the_content();
+			echo '</div>';
+			if( is_archive() )
+				echo '<header id="recent" class="archive-recent-header"><h2>Newest ' . get_the_archive_title() . '</h2></header>';
+		endwhile; endif; wp_reset_postdata();
+	}
 
- 	/**
- 	 * Get taxonomy
- 	 *
- 	 */
- 	function get_taxonomy() {
- 		$taxonomy = is_category() ? 'category' : ( is_tag() ? 'post_tag' : get_query_var( 'taxonomy' ) );
- 		if( in_array( $taxonomy, $this->supported_taxonomies ) )
- 			return $taxonomy;
- 		else
- 			return false;
- 	}
+	/**
+	 * Get taxonomy
+	 *
+	 */
+	function get_taxonomy() {
+		$taxonomy = is_category() ? 'category' : ( is_tag() ? 'post_tag' : get_query_var( 'taxonomy' ) );
+		if( in_array( $taxonomy, $this->supported_taxonomies ) )
+			return $taxonomy;
+		else
+			return false;
+	}
 
- 	/**
- 	 * Get Landing Page ID
- 	 *
- 	 */
- 	function get_landing_id() {
- 		$taxonomy = $this->get_taxonomy();
- 		if( empty( $taxonomy ) || ! is_archive() )
- 			return false;
+	/**
+	 * Get Landing Page ID
+	 *
+	 */
+	function get_landing_id() {
 
- 		$meta_key = 'be_connected_' . str_replace( '-', '_', $taxonomy );
+		if( is_post_type_archive() && in_array( get_post_type(), $this->supported_cpt_archives ) ) {
+			$loop = new WP_Query( array(
+				'post_type' => $this->post_type,
+				'posts_per_page' => 99,
+				'fields' => 'ids',
+				'no_found_rows' => true,
+				'update_post_meta_cache' => false,
+				'update_post_term_cache' => false,
+				'post_name__in' => [ 'cpt-' . get_post_type() ]
+			));
 
- 		$loop = new WP_Query( array(
- 			'post_type' => $this->post_type,
- 			'posts_per_page' => 1,
- 			'fields' => 'ids',
- 			'no_found_rows' => true,
- 			'update_post_term_cache' => false,
- 			'update_post_meta_cache' => false,
- 			'meta_query' => array(
- 				array(
- 					'key' => $meta_key,
- 					'value' => get_queried_object_id(),
- 				)
- 			)
- 		));
+		} else {
 
- 		if( empty( $loop->posts ) )
- 			return false;
- 		else
- 			return $loop->posts[0];
+			$taxonomy = $this->get_taxonomy();
+			if( empty( $taxonomy ) || ! is_archive() )
+				return false;
 
- 	}
+			$meta_key = 'be_connected_' . str_replace( '-', '_', $taxonomy );
+
+			$loop = new WP_Query( array(
+				'post_type' => $this->post_type,
+				'posts_per_page' => 1,
+				'fields' => 'ids',
+				'no_found_rows' => true,
+				'update_post_term_cache' => false,
+				'update_post_meta_cache' => false,
+				'meta_query' => array(
+					array(
+						'key' => $meta_key,
+						'value' => get_queried_object_id(),
+					)
+				)
+			));
+		}
+
+		if( empty( $loop->posts ) )
+			return false;
+		else
+			return $loop->posts[0];
+
+	}
 
 	/**
 	 * Get term link
@@ -243,40 +278,41 @@
 		return get_term_link( $term, $taxonomy );
 	}
 
- 	/**
- 	 * Admin Bar Link, Frontend
- 	 *
- 	 */
- 	 function admin_bar_link_front( $wp_admin_bar ) {
- 		 $taxonomy = $this->get_taxonomy();
- 		 if( ! $taxonomy )
- 		 	return;
+	/**
+	 * Admin Bar Link, Frontend
+	 *
+	 */
+	 function admin_bar_link_front( $wp_admin_bar ) {
+		 $taxonomy = $this->get_taxonomy();
+		 if( ! ( $taxonomy || is_post_type_archive( $this->supported_cpt_archives ) ) )
+		 	return;
 
- 		if( ! ( is_user_logged_in() && current_user_can( 'manage_categories' ) ) )
- 			return;
+		if( ! ( is_user_logged_in() && current_user_can( 'manage_categories' ) ) )
+			return;
 
- 		$archive_id = $this->get_landing_id();
- 		if( !empty( $archive_id ) ) {
- 			$wp_admin_bar->add_node( array(
- 				'id' => 'category_landing_page',
- 				'title' => 'Edit Landing Page',
- 				'href'  => get_edit_post_link( $archive_id ),
- 			) );
+		$archive_id = $this->get_landing_id();
+		$icon = '';
+		if( !empty( $archive_id ) ) {
+			$wp_admin_bar->add_node( array(
+				'id' => 'category_landing_page',
+				'title' => $icon . 'Edit Landing Page',
+				'href'  => get_edit_post_link( $archive_id ),
+			) );
 
- 		} else {
- 			$wp_admin_bar->add_node( array(
- 				'id' => 'category_landing_page',
- 				'title' => 'Add Landing Page',
- 				'href'  => admin_url( 'post-new.php?post_type=' . $this->post_type )
- 			) );
- 		}
- 	 }
+		} else {
+			$wp_admin_bar->add_node( array(
+				'id' => 'category_landing_page',
+				'title' => $icon . 'Add Landing Page',
+				'href'  => admin_url( 'post-new.php?post_type=' . $this->post_type )
+			) );
+		}
+	 }
 
-	 /**
-  	 * Admin Bar Link, Backend
-  	 *
-  	 */
-  	 function admin_bar_link_back( $wp_admin_bar ) {
+	/**
+	 * Admin Bar Link, Backend
+	 *
+	 */
+	function admin_bar_link_back( $wp_admin_bar ) {
 		if( ! is_admin() )
 			return;
 
@@ -292,23 +328,24 @@
 		if( empty( $term_link ) )
 			return;
 
+		$icon = ''
 		$wp_admin_bar->add_node( array(
 			'id'	=> 'category_landing_page',
-			'title'	=> 'View Landing Page',
+			'title'	=> $icon . 'View Landing Page',
 			'href'	=> $term_link,
 		));
-  	 }
- }
+	}
+}
 
- /**
-  * The function provides access to the class methods.
-  *
-  * Use this function like you would a global variable, except without needing
-  * to declare the global.
-  *
-  * @return object
-  */
- function category_landing_page() {
- 	return Category_Landing_Page::instance();
- }
- category_landing_page();
+/**
+ * The function provides access to the class methods.
+ *
+ * Use this function like you would a global variable, except without needing
+ * to declare the global.
+ *
+ * @return object
+ */
+function category_landing_page() {
+	return Category_Landing_Page::instance();
+}
+category_landing_page();
